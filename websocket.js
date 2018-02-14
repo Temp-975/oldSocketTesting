53819cb09da73a56cc31466d3422e55b35ed4f23
Temp-975 <36461985+Temp-975@users.noreply.github.com>
2018-02-13 21:03:26 -0500

    Update websocket.js

--- websocket.js
+++ websocket.js
@@ -3,7 +3,7 @@
       // The server will listen on port 6789 and respond back with whatever
       // has been sent to the server.
       //
-      console.log("hello");
+
        //  Request permission to listen on port 6789
        navigator.tcpServerPermission.requestPermission({"localPort":6789}).then(
          () => {
@@ -38,8 +38,8 @@
                          // Send data back
                          connected.writeable.write(value).then(
                            () => console.log("Sending data succeeded"),
-                           e => console.error("Failed to send: ", e));
-                         });
+                           e => console.error("Failed to send: ", e)
+                         );
                          // Continue to wait for data
                          waitForData ();
                        }
@@ -65,7 +65,7 @@
              () => {
                console.log("TCP server socket created sucessfully");
              },
-             e =>console.error("TCP server socket creation failed due to error: ", e);
+             e =>console.error("TCP server socket creation failed due to error: ", e)
            );
 
            // Handle TCP server closed, either as a result of the webapp
@@ -74,9 +74,9 @@
              () => {
                 console.log("TCP server socket has been cleanly closed");
              },
-             e => console.error("TCP server socket closed due to error: ", e);
+             e => console.error("TCP server socket closed due to error: ", e)
            );
 
          },
-         e => console.error("TCP Server Socket on local port 6789 was denied due to error: ", e);
+         e => console.error("TCP Server Socket on local port 6789 was denieddue to error: ", e)
        );
